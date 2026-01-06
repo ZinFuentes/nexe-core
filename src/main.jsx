7568938a7091b4beb48d8c34e5163951e32e20f7
ZinMcAlister <jfuent34@xtec.cat>
2026-01-06 01:44:22 +0000

    auth-oauth: oauth setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App.jsx';
 
-const CLIENT_ID = "481188041122-8d64eevablm9ral4cvjfl8aunfcqn7pe.apps.googleusercontent.com";
+const CLIENT_ID = "426398916508-dv5p5vmkv9vb1gs1mi87c5a00hb37uld.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteSingleFile } from "vite-plugin-singlefile"
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), viteSingleFile()],
   css: {
     postcss: {
-      plugins: [tailwindcss()],
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
-  }
-})
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
+});
